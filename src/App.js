944f9747738478a335302b83a4b1ec16906d2b5b
Rita Akhmadulina <rita.akhmadulina@gmail.com>
2016-07-28 17:48:48 +0300

    test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Welcome to React</h1>
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        
       </div>
     );
   }
